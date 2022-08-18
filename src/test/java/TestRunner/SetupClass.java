c07aa9f9a23bc5220b223f3bfe6a7d25a7e97cfc
nishaslidetech <nisha.dhiman@slidetech.in>
2022-08-18 13:02:55 +0530

    update captcha

--- src/test/java/TestRunner/SetupClass.java
+++ src/test/java/TestRunner/SetupClass.java
@@ -1,23 +1,29 @@
 package TestRunner;
+
 import java.time.Duration;
 import java.io.FileReader;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import webApp.CommonData;
 
 public class SetupClass {
@@ -28,84 +34,108 @@ public class SetupClass {
 	public static Logger log;
 	public static String Seleniumdriver;
 	public static WebElement webelement;
-	public static String localtestFF;
-	public static String localFF;
-	
-	public static String local_chromebrowser;
-	public static String local_chrome;
-	public Actions ac=new Actions(driver);
+
+	public static String browser;
+	public Actions ac = new Actions(driver);
+	public static WebDriverWait wait;
+	public static JavascriptExecutor js;
+
 	@BeforeClass
 	public static void before_Class() throws Exception {
 
 		log = Logger.getLogger(BeforeClass.class.getName());
-		property.load(new FileReader("C:\\Users\\Administrator\\eclipse-workspace\\AutoSlideTest_FFBrowser\\src\\main\\resources\\configure.properties"));
+		property.load(new FileReader("config//config.properties"));
 		AppURL = property.getProperty("App_url");
-		
-		local_chromebrowser = property.getProperty("local_chrome_browser");
-		local_chrome = property.getProperty("local_chrome");
-		localtestFF = property.getProperty("localtestFF");
-		localFF = property.getProperty("local_Fifefox_browser");
-	
 
+		// on source lab setup
 		AppURL = property.getProperty("App_url");
 		System.out.println("Bname=====" + AppURL);
 
+		if (System.getenv("browser") != null && !System.getenv("browser").isEmpty()) {
+			browser = System.getenv("browser");
+			System.out.println("env browser = " + browser);
 
+		} else {
+			browser = property.getProperty("browser");
+			System.out.println("config browser = " + browser);
+		}
 
-		if ((local_chrome.equals("yes"))) {
-			local_chromebrowser = System.setProperty(CommonData.Chrome_Name, CommonData.Chrome_Path);
+		property.setProperty("browser", browser);
+
+		System.out.println("setProperty browser = " + browser);
+
+		if ((property.getProperty("browser").equals("chrome"))) {
+			WebDriverManager.chromedriver().setup();
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--disable-notifications");
 			driver = new ChromeDriver(options);
 			driver.manage().window().maximize();
-	
-		} else if ((localtestFF.equals("yes"))) {
-
-			localFF = System.setProperty(CommonData.Firefox_Name, CommonData.Firefox_Path);
-			FirefoxProfile profile = new FirefoxProfile();
-			profile.setPreference("dom.webnotifications.enabled", false);
-		   driver = new FirefoxDriver();
-			driver.manage().window().maximize();
-		   Thread.sleep(1000);
-
+			wait = new WebDriverWait(driver, 50);
+			js = (JavascriptExecutor) driver;
+			Thread.sleep(1000);
 		}
-		
+		// if (browser.equalsIgnoreCase("firefox"))
+
+		// if (browser.equalsIgnoreCase("chrome"))
+		else if ((property.getProperty("browser").equals("firefox"))) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+			driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+			wait = new WebDriverWait(driver, 50);
+			js = (JavascriptExecutor) driver;
+			Thread.sleep(1000);
+		} else if ((property.getProperty("browser").equals("edge"))) {
+
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+			driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+			wait = new WebDriverWait(driver, 50);
+			js = (JavascriptExecutor) driver;
+			Thread.sleep(1000);
+		}
+
 		else {
-			System.out.println("browser is not open");
-			
+
+			System.out.println("platform does not provide");
 		}
 
 	}
+
 	
+
 	public static WebElement visibilityofelement(By locator) {
 		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
 				// Check for condition in every 2 seconds
 				.pollingEvery(Duration.ofSeconds(2))
 				// Till time out i.e. 30 seconds
-				.withTimeout(Duration.ofSeconds(30))
-				.ignoring(NoSuchElementException.class);
+				.withTimeout(Duration.ofSeconds(30)).ignoring(NoSuchElementException.class);
 		return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 	}
-	
+
 	public static Boolean waitForElementText(By locator, String text) {
 		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
 				// Check for condition in every 2 seconds
 				.pollingEvery(Duration.ofSeconds(2))
 				// Till time out i.e. 30 seconds
-				.withTimeout(Duration.ofSeconds(30))
-				.ignoring(NoSuchElementException.class);
+				.withTimeout(Duration.ofSeconds(30)).ignoring(NoSuchElementException.class);
 		return wait.until(ExpectedConditions.textToBe(locator, text));
 	}
-	
 
 	@AfterClass
 
 	public static void after_Class() {
-try {
-		driver.quit();
-		Thread.sleep(2000);
-}catch (Exception closeBrowser) {
-	
-}
+		/*
+		 * try { driver.quit(); Thread.sleep(2000); } catch (Exception closeBrowser) {
+		 * 
+		 * }
+		 */
 	}
 }
--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -2,25 +2,24 @@ package TestRunner.loginSteps;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.lang.String;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.JavascriptExecutor;
 
 import TestRunner.SetupClass;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
 public class SignUp_Step extends SetupClass {
-	
-	WebDriverWait wait = new WebDriverWait(driver,50);
-         JavascriptExecutor js = (JavascriptExecutor) driver;
-	
+
+	WebDriverWait wait = new WebDriverWait(driver, 50);
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+
 	@Given("^user is already on Website Home Page ii$")
 	public void user_is_already_on_Website_Home_Page_ii() throws Throwable {
 		driver.get("https://www.slidegeeks.com/");
@@ -30,7 +29,6 @@ public class SignUp_Step extends SetupClass {
 		Thread.sleep(4000);
 		driver.navigate().refresh();
 		Thread.sleep(4000);
-		
 
 		Thread.sleep(3000);
 
@@ -42,20 +40,21 @@ public class SignUp_Step extends SetupClass {
 				"div[id='Individual'] form[name='hikashop_product_form_205548_hikashop_category_information_menu_117']")));
 
 		Join_now.click();
-		Thread.sleep(3000);
-		
+		Thread.sleep(5000);
+
 		WebElement signup = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='signupclass']")));
 
 		signup.click();
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 
-		/*WebElement name = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='site_signup_name']")));
-		Thread.sleep(3000);
-		name.sendKeys("Automated Program");
-		Thread.sleep(3000);*/
-		
-		//without pop-up
+		/*
+		 * WebElement name = wait
+		 * .until(ExpectedConditions.elementToBeClickable(By.xpath(
+		 * "//input[@id='site_signup_name']"))); Thread.sleep(3000);
+		 * name.sendKeys("Automated Program"); Thread.sleep(3000);
+		 */
+
+		// without pop-up
 		WebElement name = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#register_name")));
 		Thread.sleep(3000);
 		name.sendKeys("Automated Program");
@@ -80,34 +79,39 @@ public class SignUp_Step extends SetupClass {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		Thread.sleep(2000);
 
-		/*WebElement new_email = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='site_signup_email']")));
+		/*
+		 * WebElement new_email = wait
+		 * .until(ExpectedConditions.elementToBeClickable(By.xpath(
+		 * "//input[@id='site_signup_email']")));
+		 * 
+		 * new_email.sendKeys(full_email); Thread.sleep(3000);
+		 * 
+		 * WebElement password = wait
+		 * .until(ExpectedConditions.elementToBeClickable(By.xpath(
+		 * "//input[@id='site_signup_password']")));
+		 * 
+		 * password.sendKeys("Geeks@123");
+		 * 
+		 * WebElement captcha =
+		 * wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+		 * "//input[@id='captchtext']")));
+		 * 
+		 * captcha.sendKeys("Y3Tt6bfwI");
+		 * 
+		 * WebElement selectRadioBtn = wait
+		 * .until(ExpectedConditions.elementToBeClickable(By.xpath(
+		 * "//input[@id='site_signup_checkbox']")));
+		 * 
+		 * selectRadioBtn.click();
+		 * 
+		 * WebElement register_btn = wait
+		 * .until(ExpectedConditions.elementToBeClickable(By.xpath(
+		 * "//button[@id='site_signup_btn']"))); Thread.sleep(3000);
+		 * register_btn.click(); Thread.sleep(5000);
+		 */
 
-		new_email.sendKeys(full_email);
-		Thread.sleep(3000);
+		// without pop-up
 
-		WebElement password = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='site_signup_password']")));
-
-		password.sendKeys("Geeks@123");
-
-		WebElement captcha = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='captchtext']")));
-
-		captcha.sendKeys("Y3Tt6bfwI");
-
-		WebElement selectRadioBtn = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='site_signup_checkbox']")));
-
-		selectRadioBtn.click();
-
-		WebElement register_btn = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='site_signup_btn']")));
-		Thread.sleep(3000);
-		register_btn.click();
-		Thread.sleep(5000);*/
-		
-	// without pop-up
-		
 		WebElement new_email = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#register_email")));
 		Thread.sleep(3000);
 		new_email.sendKeys(full_email);
@@ -138,73 +142,61 @@ public class SignUp_Step extends SetupClass {
 
 	@Then("^Stripe Checkout$")
 	public void Stripe_Checkout() throws Throwable {
-		
-		//driver.get("https://www.slidegeeks.com/subscriptions");
-		//Thread.sleep(3000);
-		// WebElement Join_now = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#Monthly")));
-		// Join_now.click();
-		//Thread.sleep(5000);
-		//String URL = driver.getCurrentUrl();
-               // Assert.assertEquals(URL, "https://www.slidegeeks.com/component/pago/checkout");
-		//System.out.println("URL matching --> Part executed");
-		//Thread.sleep(5000);
-		 try {
-		Thread.sleep(2000);
-		// select stripe option
-             WebElement cp_btn  = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/input[1]"));
-		//WebElement co_btn  =  wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pg-checkout-billing-payment-form > div > div:nth-child(1) > label")));
-	        cp_btn.click();
-		Thread.sleep(2000);
-	     } catch( NoSuchElementException popup) { 
-	     }
-		
-		// place order button 
 		try {
-			
-		 WebElement place_order_btn  = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#hikabtn_checkout_next")));
 			Thread.sleep(2000);
-			js.executeScript("arguments[0].scrollIntoView();",place_order_btn);	
-			//js.executeScript("arguments[0].click();", place_order_btn);
+			// select stripe option
+			WebElement cp_btn = driver.findElement(By.xpath(
+					"/html[1]/body[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/input[1]"));
+			cp_btn.click();
+			Thread.sleep(2000);
+		} catch (NoSuchElementException popup) {
+		}
+
+		// place order button
+		try {
+
+			WebElement place_order_btn = wait
+					.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#hikabtn_checkout_next")));
+			Thread.sleep(2000);
+			js.executeScript("arguments[0].scrollIntoView();", place_order_btn);
+			// js.executeScript("arguments[0].click();", place_order_btn);
 			Thread.sleep(1000);
-		    place_order_btn.click();
+			place_order_btn.click();
 			Thread.sleep(3000);
 		} catch (NoSuchElementException popup) {
 		}
-		
-	
+
 	}
+
 	@Then("^Stripe Checkout Redirection$")
 	public void Stripe_Checkout_Redirection() throws Throwable {
-	 String stripe_page_title=driver.getTitle();
+		String stripe_page_title = driver.getTitle();
 		Thread.sleep(3000);
-	    System.out.println("Title of the Page is --> "+stripe_page_title);
-	    
-	    String page_title="https://checkout.stripe.com/";
-	    
-	    if(page_title.equalsIgnoreCase(stripe_page_title))
-	    {
-	    	System.out.println(" user is on the Stripe page");
-	    	log.info("USER IS ON THE STRIPE PAGE");
-	    }
-	    else
-	    {
-	    	System.out.println("user is on the wrong page");
-	    	log.info("USER IS ON THE WRONG PAGE");
-	    }	
-		
-		Thread.sleep(1000);
-		 WebElement Stripe_email = driver.findElement(By.cssSelector("#email"));
-		 Stripe_email.sendKeys("slidetech.qa@gmail.com");
-		Thread.sleep(2000);
-		 WebElement Stripe_back = driver.findElement(By.cssSelector("#root > div > div > div.App-Overview > header > div > div > a > div > div > div.Header-backArrowContainer > svg"));
-		Thread.sleep(2000);
-			Stripe_back.click();
-		Thread.sleep(5000);
-		//driver.get("https://www.slidegeeks.com/component/pago/checkout");
-		//Thread.sleep(2000);
-		
+		System.out.println("Title of the Page is --> " + stripe_page_title);
 
-                 WebElement Account = driver.findElement(
+		String page_title = "https://checkout.stripe.com/";
+
+		if (page_title.equalsIgnoreCase(stripe_page_title)) {
+			System.out.println(" user is on the Stripe page");
+			log.info("USER IS ON THE STRIPE PAGE");
+		} else {
+			System.out.println("user is on the wrong page");
+			log.info("USER IS ON THE WRONG PAGE");
+		}
+
+		Thread.sleep(1000);
+		WebElement Stripe_email = driver.findElement(By.cssSelector("#email"));
+		Stripe_email.sendKeys("slidetech.qa@gmail.com");
+		Thread.sleep(2000);
+		WebElement Stripe_back = driver.findElement(By.cssSelector(
+				"#root > div > div > div.App-Overview > header > div > div > a > div > div > div.Header-backArrowContainer > svg"));
+		Thread.sleep(2000);
+		Stripe_back.click();
+		Thread.sleep(5000);
+		// driver.get("https://www.slidegeeks.com/component/pago/checkout");
+		// Thread.sleep(2000);
+
+		WebElement Account = driver.findElement(
 				By.xpath("/html/body/div[1]/header/div/div/nav/div/div[2]/div[2]/div[2]/div/div[2]/ul/li[1]/a"));
 		Thread.sleep(3000);
 		Account.click();
@@ -236,7 +228,7 @@ public class SignUp_Step extends SetupClass {
 		System.out.println("verifyText1 = " + verifyDeleteAccountMessage);
 		Assert.assertTrue("Your are not on paypal page",
 				verifyDeleteAccountMessage.contentEquals("Your Account has been deleted successfully."));
-		Thread.sleep(3000);						    
+		Thread.sleep(3000);
 	}
 
 }
